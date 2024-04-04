24344a69abfd0b312bb2054368ba227522cbbc9e
conortoole <70600055+conortoole@users.noreply.github.com>
2024-04-03 20:18:48 -0700

    Add files via upload

--- RIMmain.ino
+++ RIMmain.ino
@@ -1,22 +1,49 @@
 #include <Arduino.h>
 #include <Adafruit_TinyUSB.h>
 #include <SoftwareSerial.h>
-#include <rimGPS.h>
+//#include <rimGPS.h>
 #include <TinyGPSPlus.h>
 
- rimGPS gps; //remote ID module GPS 
+ //rimGPS gps; //remote ID module GPS 
+
+//void setup() {
+//  SoftwareSerial serial(A0, A1);
+  
+//  gps.initialize(serial, 9600); //comunicate to GPS
+//  Serial.begin(115200); //comunicate to monitor
+     
+//}
+
+//void loop() {
+ // delay(1000);
+  //Serial.println(gps.location.lat());
+  // put your main code here, to run repeatedly:
+
+//}
+
+
+TinyGPSPlus gps;
+SoftwareSerial serial(A0, A1);
 
 void setup() {
-  SoftwareSerial serial(A0, A1);
-  
-  gps.initialize(serial, 9600); //comunicate to GPS
+  serial.begin(9600);
   Serial.begin(115200); //comunicate to monitor
-     
 }
 
 void loop() {
-  delay(1000);
-  Serial.println(gps.location.lat());
-  // put your main code here, to run repeatedly:
+     while (serial.available() > 0) {
+          if(gps.encode(serial.read())){
+               Serial.println(gps.location.lat());
+               Serial.println(gps.location.lng());
+               Serial.print(gps.time.hour());
+               Serial.print(":");
+               Serial.println(gps.time.minute());
+          }
+     }
+
+     if (millis() > 5000 && gps.charsProcessed() < 10) {
+          Serial.println(("No GPS detected: check wiring."));
+          while(true);
+     }
 
 }
